0c9b492fdd5528a5bc6ca542139eda22b208e1c7
Rohitendra <rohitendra.singh75@gmail.com>
2024-03-07 09:03:15 +0530

    Updated Again

--- src/Components/Usercard/Usercard.jsx
+++ src/Components/Usercard/Usercard.jsx
@@ -6,7 +6,7 @@ export const Usercard = () => {
     <div class="upc">
     <div class="gradient"></div>
     <div class="profile-icon">
-        <img src={user_image} >
+        <img src={user_image} alt="" />
         <div class="profile-title">Zenin Maki</div>
         <div class="profile-sedcription">I,m the most powerfull warrior in Zenin Clan.</div>
         <div class="profile-button">
